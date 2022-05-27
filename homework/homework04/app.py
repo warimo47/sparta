bb44bcdfc83ab06d96d43bcc969e60f78332da5f
KangsanBae <warimo47@gmail.com>
2022-05-27 15:32:32 +0900

    The end of a lecture

--- homework/homework04/app.py
+++ homework/homework04/app.py
@@ -3,7 +3,8 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-client = MongoClient('localhost', 27017)
+client = MongoClient('mongodb://test:test@localhost', 27017)
+# client = MongoClient('localhost', 27017)
 db = client.dbhomework
 
 
